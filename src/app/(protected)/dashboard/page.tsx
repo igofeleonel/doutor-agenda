1e237e18dc93b4e5392fb717ac84a19de0e2d3a4
mari <marigorsamuel@gmail.com>
2025-06-14 14:02:44 -0300

    feat: add today appointments table

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import dayjs from "dayjs";
 import { and, count, desc, eq, gte, lte, sql, sum } from "drizzle-orm";
+import { Calendar } from "lucide-react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTable } from "@/components/ui/data-table";
 import {
   PageActions,
   PageContainer,
@@ -16,6 +19,7 @@ import { db } from "@/db";
 import { appointmentsTable, doctorsTable, patientsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
+import { appointmentsTableColumns } from "../appointments/_components/table-columns";
 import RevenueChart from "./_components/appointments-chart";
 import { DatePicker } from "./_components/date-picker";
 import StatsCards from "./_components/stats-cards";
@@ -66,6 +70,7 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
     [totalDoctors],
     topDoctors,
     topSpecialties,
+    todayAppointments,
   ] = await Promise.all([
     db
       .select({ total: sum(appointmentsTable.appointmentPriceInCents) })
@@ -132,6 +137,17 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
       )
       .groupBy(doctorsTable.specialty)
       .orderBy(desc(count(appointmentsTable.id))),
+    db.query.appointmentsTable.findMany({
+      where: and(
+        eq(appointmentsTable.clinicId, session.user.clinic.id),
+        gte(appointmentsTable.date, dayjs().startOf("day").toDate()),
+        lte(appointmentsTable.date, dayjs().endOf("day").toDate()),
+      ),
+      with: {
+        doctor: true,
+        patient: true,
+      },
+    }),
   ]);
 
   const chartStartDate = dayjs(from).startOf("day").toDate();
@@ -170,6 +186,7 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
           <DatePicker />
         </PageActions>
       </PageHeader>
+
       <PageContent>
         <StatsCards
           totalRevenue={totalRevenue.total ? Number(totalRevenue.total) : null}
@@ -177,12 +194,30 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
           totalPatients={totalPatients.total}
           totalDoctors={totalDoctors.total}
         />
+
         <div className="grid grid-cols-[2.25fr_1fr] gap-4">
           <RevenueChart dailyAppointmentsData={dailyAppointmentsData} />
           <TopDoctors doctors={topDoctors} />
         </div>
+
         <div className="grid grid-cols-[2.25fr_1fr] gap-4">
-          {/* Tabela */}
+          <Card>
+            <CardHeader>
+              <div className="flex items-center gap-3">
+                <Calendar className="text-muted-foreground" />
+                <CardTitle className="text-base">
+                  Agendamentos de hoje
+                </CardTitle>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <DataTable
+                columns={appointmentsTableColumns}
+                data={todayAppointments}
+              />
+            </CardContent>
+          </Card>
+
           <TopSpecialties topSpecialties={topSpecialties} />
         </div>
       </PageContent>
